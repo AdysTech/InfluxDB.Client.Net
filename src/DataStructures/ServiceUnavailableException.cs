1ba9f4fe40de743cf6582fdb91c9aeaa410bece4
mvadu <adarsha.mv@gmail.com>
2017-03-06 19:52:15 -0800

    Add Support for .Net Core and Travis CI integration

--- src/DataStructures/ServiceUnavailableException.cs
+++ src/DataStructures/ServiceUnavailableException.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdysTech.InfluxDB.Client.Net
 {
-    [Serializable]
     public class ServiceUnavailableException:InvalidOperationException
     {
         public ServiceUnavailableException():base("InfluxDB service is not available")
