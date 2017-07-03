d3f120a543e7ce60c0c51ef4b3a2e47118717208
Panagiotis Rikarnto Siavelis <siavelis@ceid.upatras.gr>
2017-07-03 11:57:40 +0300

    Escape double quotes in string fields. Updated data generation in test cases  (fixes #36)

--- tests/DataGen.cs
+++ tests/DataGen.cs
@@ -8,63 +8,63 @@ namespace AdysTech.InfluxDB.Client.Net.Tests
 {
     static class DataGen
     {
-        private static readonly Random random = new Random ();
-        private static readonly object syncLock = new object ();
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
 
         public static int RandomNumber(int min, int max)
         {
-            lock ( syncLock )
+            lock (syncLock)
             { // synchronize
-                return random.Next (min, max);
+                return random.Next(min, max);
             }
         }
 
         public static int RandomInt()
         {
-            lock ( syncLock )
+            lock (syncLock)
             { // synchronize
-                return random.Next ();
+                return random.Next();
             }
         }
 
         public static int RandomInt(int Max)
         {
-            lock ( syncLock )
+            lock (syncLock)
             { // synchronize
-                return random.Next (Max);
+                return random.Next(Max);
             }
         }
 
         public static double RandomDouble()
         {
-            lock ( syncLock )
+            lock (syncLock)
             { // synchronize
-                return random.NextDouble ();
+                return random.NextDouble();
             }
         }
 
         public static string RandomString()
         {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ,=/";
-            var stringChars = new StringBuilder (16, 16);
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ,=/\"";
+            var stringChars = new StringBuilder(16, 16);
 
-            for ( int i = 0; i < stringChars.Capacity; i++ )
+            for (int i = 0; i < stringChars.Capacity; i++)
             {
-                lock ( syncLock )
+                lock (syncLock)
                 {
-                    stringChars.Append(chars[random.Next (chars.Length)]);
+                    stringChars.Append(chars[random.Next(chars.Length)]);
                 }
             }
-            return stringChars.ToString (); ;
+            return stringChars.ToString(); ;
         }
 
         public static DateTime RandomDate(DateTime from, DateTime to)
         {
-            lock ( syncLock )
+            lock (syncLock)
             {
-                return from.AddTicks ((long) ( random.NextDouble () * ( to.Ticks - from.Ticks ) ));
+                return from.AddTicks((long)(random.NextDouble() * (to.Ticks - from.Ticks)));
             }
         }
     }
-  
+
 }
