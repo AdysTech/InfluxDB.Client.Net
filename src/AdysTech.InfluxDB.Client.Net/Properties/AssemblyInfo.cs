045a323fde4ec4c66c86c30b4edaeaa7360bece3
mvadu <8618235+mvadu@users.noreply.github.com>
2018-07-11 23:08:53 -0400

    Minor Update
    Update Travis CI setup, copyright, minor version info, change log.

--- src/AdysTech.InfluxDB.Client.Net/Properties/AssemblyInfo.cs
+++ src/AdysTech.InfluxDB.Client.Net/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AdysTech")]
 [assembly: AssemblyProduct("AdysTech.InfluxDB.Client.Net")]
-[assembly: AssemblyCopyright("Copyright © AdysTech 2016-2017")]
+[assembly: AssemblyCopyright("Copyright © AdysTech 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.7.0")]
-[assembly: AssemblyFileVersion("0.6.7.0")]
+[assembly: AssemblyVersion("0.6.8.0")]
+[assembly: AssemblyFileVersion("0.6.8.0")]
